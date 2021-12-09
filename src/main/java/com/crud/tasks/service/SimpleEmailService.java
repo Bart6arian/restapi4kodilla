207cd565c76f81b97e7383820f889a4752af7ea6
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-12-09 11:03:07 +0100

    23.3 update

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,11 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Mail;
-import com.crud.tasks.exception.EmailReceiverException;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -27,22 +23,17 @@ public class SimpleEmailService {
             log.info("Email has been sent. ");
         }catch (MailException e) {
             log.error("Failed to process email sending: " + e.getMessage(), e);
-        } catch (EmailReceiverException e) {
-            e.printStackTrace();
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail) throws EmailReceiverException {
+    private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-
         if(mail.getToCc() != null) {
             mailMessage.setCc(mail.getToCc());
-        } else
-            throw new EmailReceiverException("Incorrect input of: " + mail.getToCc());
-
+        }
         return mailMessage;
     }
 
