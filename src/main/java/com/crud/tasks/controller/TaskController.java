0fe1edbc36604be6eb3bd56300f984776377d232
Bartosz Zemsta <zemstabartosz@gmail.com>
2022-02-18 11:08:44 +0100

    33.4 REST API refcator

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TaskController {
 
     private final DbService dbService;
